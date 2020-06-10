ec8b63e42119a5f8cb143c2228bc05917bc97df5
Ben Abbatematteo <abba@brown.edu>
2020-06-10 12:36:39 -0400

    reset method actually resetting now. action space including gripper. also, gravity compensation with sim.data.qfrc_bias such that the arm does not just immediately fall down.

--- motor_skills/envs/mj_jaco/MjJacoDoor.py
+++ motor_skills/envs/mj_jaco/MjJacoDoor.py
@@ -13,15 +13,15 @@ class MjJacoDoor(gym.Env):
     """docstring for MjJacoDoor."""
 
     def __init__(self, vis=False, n_steps=int(1000)):
-        parent_dir_path = str(pathlib.Path(__file__).parent.absolute())
+        self.parent_dir_path = str(pathlib.Path(__file__).parent.absolute())
         self.vis=vis
-        self.fname = parent_dir_path + '/assets/kinova_j2s6s300/mj-j2s6s300_door.xml'
+        self.fname = self.parent_dir_path + '/assets/kinova_j2s6s300/mj-j2s6s300_door.xml'
         self.model = load_model_from_path(self.fname)
         self.sim = MjSim(self.model)
         self.viewer = MjViewer(self.sim) if self.vis else None
 
-        a_low = np.full(6, -float('inf'))
-        a_high = np.full(6, float('inf'))
+        a_low = np.full(12, -float('inf'))
+        a_high = np.full(12, float('inf'))
         self.action_space = gym.spaces.Box(a_low,a_high)
 
         obs_space = self.model.nq + self.model.nsensordata
@@ -31,20 +31,32 @@ class MjJacoDoor(gym.Env):
         self.env=self
         self.n_steps = n_steps
 
-        start_pose_file = open(parent_dir_path + "/assets/MjJacoDoorGrasps", 'rb')
+
+    def reset(self):
+
+        # pick a random start arm pose (DoFs 1-6)
+        start_pose_file = open(self.parent_dir_path + "/assets/MjJacoDoorGrasps", 'rb')
         self.start_poses = pickle.load(start_pose_file)
         idx = np.random.randint(len(self.start_poses))
         self.sim.data.qpos[:6]=self.start_poses[idx]
         self.sim.step()
 
-    def reset(self):
+        # TODO: close the gripper here
+
+        # reset the object
+        self.sim.data.qpos[-1]=0.0
+        self.sim.data.qpos[-2]=0.0
+
         self.elapsed_steps=0
         obs = np.concatenate([self.sim.data.qpos, self.sim.data.sensordata])
         return obs
 
     def step(self, action):
+
+        # TODO: failure predicate here
+
         for i in range(len(action)):
-            self.sim.data.ctrl[i]=action[i]
+            self.sim.data.ctrl[i]=action[i]+self.sim.data.qfrc_bias[i]
 
         self.sim.forward()
         self.sim.step()
@@ -54,7 +66,7 @@ class MjJacoDoor(gym.Env):
 
         info={'goal_achieved': reward}
 
-        done = self.elapsed_steps == self.n_steps - 1
+        done = self.elapsed_steps == (self.n_steps - 1)
 
         obs = np.concatenate([self.sim.data.qpos, self.sim.data.sensordata])
 

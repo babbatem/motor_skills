3f2814bda73d39a2ef4a95271d415d670cb4315e
Nathan Tung <nathantung@Nathans-MacBook-Pro.local>
2020-06-22 12:37:12 -0600

    position grasper and close fingers

--- motor_skills/envs/mj_jaco/MjJacoDoor.py
+++ motor_skills/envs/mj_jaco/MjJacoDoor.py
@@ -6,6 +6,8 @@ import gym
 import numpy as np
 from mujoco_py import load_model_from_path, MjSim, MjViewer
 from motor_skills.envs.mj_jaco import MjJacoEnv
+import motor_skills.core.mj_control as mjc
+from mujoco_py import cymj
 
 THRESHOLD = np.pi / 2
 
@@ -38,13 +40,41 @@ class MjJacoDoor(gym.Env):
         start_pose_file = open(self.parent_dir_path + "/assets/MjJacoDoorGrasps", 'rb')
         self.start_poses = pickle.load(start_pose_file)
         idx = np.random.randint(len(self.start_poses))
-        self.sim.data.qpos[:6]=self.start_poses[idx]
+        # self.sim.data.qpos[:6]=self.start_poses[idx]
+        self.sim.data.qpos[:6] = self.start_poses[8]
         self.sim.step()
 
         # TODO: close the gripper here
         # a heuristic strategy: close fingers until the first link is in contact.
         # then close finger tips in the same fashion
 
+        ee_index = 6
+        ee_xpos_goal = copy.deepcopy(self.sim.data.body_xpos[ee_index])
+        ee_xpos_goal[1] += 0.2
+        ee_xpos_goal[2] -= 0.005
+
+        for t in range(1000):
+            self.sim.data.ctrl[:] = mjc.ee_regulation(ee_xpos_goal, self.sim, ee_index, kp=None, kv=None, ndof=12)
+            self.sim.step()
+            self.sim.forward()
+            self.viewer.render()
+
+        print(self.sim.data.qpos)
+        obj_type = 3 # 3 for joint, 1 for body
+        joint_idxs = np.array([])
+        for i in range(1,4):
+            base_idx = cymj._mj_name2id(self.sim.model, obj_type,"j2s6s300_joint_finger_" + str(i))
+            tip_idx = cymj._mj_name2id(self.sim.model, obj_type,"j2s6s300_joint_finger_tip_" + str(i))
+            offset = np.zeros(12)
+            offset[base_idx] = 2
+            offset[tip_idx] = 2
+            new_pos = self.sim.data.qpos[:12] + offset
+            for t in range(100):
+                self.sim.data.ctrl[:] = mjc.pd([0] * 12, [0] * 12, new_pos, self.sim, ndof=12)
+                self.sim.step()
+                self.sim.forward()
+                self.viewer.render()
+                print(self.sim.data.sensordata)
         # reset the object
         self.sim.data.qpos[-1]=0.0
         self.sim.data.qpos[-2]=0.0

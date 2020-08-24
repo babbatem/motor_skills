bcda9d039826d65dc923e96e3cc8b36848507556
Ben Abbatematteo <abba@brown.edu>
2020-08-24 17:44:29 -0400

    plannerwrapper consumes env now, not sim

--- motor_skills/cip/pbplannerWrapper.py
+++ motor_skills/cip/pbplannerWrapper.py
@@ -9,8 +9,8 @@ from motor_skills.planner.pbplanner import PbPlanner
 from motor_skills.cip.arm_controller import PositionOrientationController
 import motor_skills.core.mj_control as mjc
 
-MIN_PLANNER_STEPS = 100 # minimum length of the plan
-EXTRA_PLANNER_STEPS = 100 # steps to let the arm converge after executing planned trajectory
+MIN_PLANNER_STEPS = 1000 # minimum length of the plan
+EXTRA_PLANNER_STEPS = 200 # steps to let the arm converge after executing planned trajectory
 ARMDOF = 6
 GRIPPERDOF = 6
 
@@ -20,22 +20,12 @@ class pbplannerWrapper(object):
 		takes the pbplanner, purely pybullet, and provides basic trajectory
 		execution in mujoco.
 	"""
-	def __init__(self):
-		super(pbplannerWrapper, self).__init__()
-
-		# %% load the controller (TODO: maybe the CIP object here)
-		# with open(controller_file) as f:
-		# 	params = hjson.load(f)
-		#
-		# params['position_orientation']['impedance_flag']=False
-		#
-		# self.sim = si
-		# self.controller = PositionOrientationController(**params['position_orientation'])
+	def __init__(self, debug=False):
+		self.planner = PbPlanner()
 		self.target_q = None
 		self.target_qd = None
+		self.debug=debug
 
-		# %% load that planner
-		self.planner = PbPlanner()
 
 	def plan(self, s, g):
 		"""
@@ -45,11 +35,7 @@ class pbplannerWrapper(object):
 
 		sets self.target_q and self.target_qd
 		"""
-		# s = planner.validityChecker.sample_state()
-		# g = planner.validityChecker.sample_state()
 		result=self.planner.plan(s, g)
-
-		# % TODO: finish me
 		result.interpolate(MIN_PLANNER_STEPS)
 		H = result.getStateCount()
 		self.target_q = []
@@ -61,43 +47,39 @@ class pbplannerWrapper(object):
 			self.target_q.append(tmp)
 
 		self.target_q = np.array(self.target_q)
-		# self.target_qd = np.diff(self.target_q, axis=0)
-		# self.target_qd = np.append(self.target_qd, np.zeros((1,6)), axis=0)
 		self.target_qd = np.zeros_like(self.target_q)
 
-		print('----')
-		print(self.target_q.shape)
-		print(self.target_qd.shape)
-
-
-	def execute(self, sim):
+	def execute(self, env):
 		# TODO: control frequency?
 
-		# % execute self.target_q
+		# % execute self.target_q waypoints
 		H = len(self.target_q)
 		for i in range(H):
-			torques=mjc.pd(None, self.target_qd[i], self.target_q[i], sim,
+			torques=mjc.pd(None, self.target_qd[i], self.target_q[i], env.sim,
 						   ndof=6, kp=np.eye(6)*300)
-			sim.data.ctrl[:6]=torques
-			sim.step()
-			env.viewer.render()
-			time.sleep(0.1)
+			env.sim.data.ctrl[:6]=torques
+			env.sim.step()
 
-		# % let arm converge. 
+			if self.debug:
+				env.viewer.render()
+				# time.sleep(0.1)
+		# % let arm converge.
 		for j in range(EXTRA_PLANNER_STEPS):
-			torques=mjc.pd(None, self.target_qd[i], self.target_q[i], sim,
+			torques=mjc.pd(None, self.target_qd[i], self.target_q[i], env.sim,
 						   ndof=6, kp=np.eye(6)*300)
-			sim.data.ctrl[:6]=torques
-			sim.step()
-			env.viewer.render()
-			time.sleep(0.1)
+			env.sim.data.ctrl[:6]=torques
+			env.sim.step()
+
+			if self.debug:
+				env.viewer.render()
+				# time.sleep(0.1)
 
 
 if __name__ == '__main__':
 
 	from motor_skills.envs.mj_jaco import MjJacoDoorImpedance
 
-	wrap = pbplannerWrapper()
+	wrap = pbplannerWrapper(debug=True)
 	g = wrap.planner.validityChecker.sample_state()
 
 	env = MjJacoDoorImpedance(vis=True)
@@ -105,7 +87,7 @@ if __name__ == '__main__':
 
 	s = copy.deepcopy(env.sim.data.qpos[:6])
 	wrap.plan(s, g)
-	wrap.execute(env.sim)
+	wrap.execute(env)
 
 	#
 	# while True:

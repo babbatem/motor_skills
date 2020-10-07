9181a43819bf7aaf79c0c5bcfc01e247f0872fe1
Ben Abbatematteo <abba@brown.edu>
2020-10-07 11:28:44 -0400

    tabs n spaces

--- motor_skills/envs/mj_jaco/mj_cip_utils.py
+++ motor_skills/envs/mj_jaco/mj_cip_utils.py
@@ -33,38 +33,38 @@ def sample_random_pose(sim, model):
 
 
 def door_open_success(sim):
-    return sim.data.qpos[-2] >= (DOOR_GOAL_OPEN - EPSILON)
+	return sim.data.qpos[-2] >= (DOOR_GOAL_OPEN - EPSILON)
 
 def door_closed_success(sim):
-    return sim.data.qpos[-2] <= (DOOR_GOAL_CLOSED + EPSILON)
+	return sim.data.qpos[-2] <= (DOOR_GOAL_CLOSED + EPSILON)
 
 def dense_open_cost(sim):
-    gripper_idx = cymj._mj_name2id(sim.model, 1, "j2s6s300_link_6")
-    handle_idx  = cymj._mj_name2id(sim.model, 1, "latch")
-    gripper_handle_displacement = np.array(sim.data.body_xpos[gripper_idx]) - \
-                    		      np.array(sim.data.body_xpos[handle_idx])
+	gripper_idx = cymj._mj_name2id(sim.model, 1, "j2s6s300_link_6")
+	handle_idx  = cymj._mj_name2id(sim.model, 1, "latch")
+	gripper_handle_displacement = np.array(sim.data.body_xpos[gripper_idx]) - \
+								  np.array(sim.data.body_xpos[handle_idx])
 
-    gripper_handle_distance = np.linalg.norm(gripper_handle_displacement)
-    # cost = DOOR_WEIGHT*(DOOR_GOAL_OPEN - max(sim.data.qpos[-2], 0) )**2 + \
-    #      HANDLE_WEIGHT*(HANDLE_GOAL_OPEN - sim.data.qpos[-1])**2 + \
-    #      GRASP_WEIGHT*(gripper_handle_distance)  # note: I added the max( . , 0) to prevent penalizing for pushing the door forward.
+	gripper_handle_distance = np.linalg.norm(gripper_handle_displacement)
+	# cost = DOOR_WEIGHT*(DOOR_GOAL_OPEN - max(sim.data.qpos[-2], 0) )**2 + \
+	#      HANDLE_WEIGHT*(HANDLE_GOAL_OPEN - sim.data.qpos[-1])**2 + \
+	#      GRASP_WEIGHT*(gripper_handle_distance)  # note: I added the max( . , 0) to prevent penalizing for pushing the door forward.
 
 	cost = DOOR_WEIGHT*(DOOR_GOAL_OPEN - sim.data.qpos[-2])**2 + \
-           HANDLE_WEIGHT*(HANDLE_GOAL_OPEN - sim.data.qpos[-1])**2 + \
-           GRASP_WEIGHT*(gripper_handle_distance)  # note: I added the max( . , 0) to prevent penalizing for pushing the door forward.
-    return cost
+		   HANDLE_WEIGHT*(HANDLE_GOAL_OPEN - sim.data.qpos[-1])**2 + \
+		   GRASP_WEIGHT*(gripper_handle_distance)  # note: I added the max( . , 0) to prevent penalizing for pushing the door forward.
+	return cost
 
 def dense_closing_cost(sim):
-    gripper_idx = cymj._mj_name2id(sim.model, 1, "j2s6s300_link_6")
-    handle_idx  = cymj._mj_name2id(sim.model, 1, "latch")
-    gripper_handle_displacement = sim.data.body_xpos[gripper_idx] - \
-                    sim.data.body_xpos[handle_idx]
+	gripper_idx = cymj._mj_name2id(sim.model, 1, "j2s6s300_link_6")
+	handle_idx  = cymj._mj_name2id(sim.model, 1, "latch")
+	gripper_handle_displacement = sim.data.body_xpos[gripper_idx] - \
+					sim.data.body_xpos[handle_idx]
 
-    gripper_handle_distance = np.linalg.norm(gripper_handle_displacement)
-    cost = DOOR_WEIGHT*(DOOR_GOAL_ - sim.data.qpos[-2])**2 + \
-         HANDLE_WEIGHT*(HANDLE_GOAL_CLOSED - sim.data.qpos[-1])**2 + \
-         GRASP_WEIGHT*(gripper_handle_distance)
-    return cost
+	gripper_handle_distance = np.linalg.norm(gripper_handle_displacement)
+	cost = DOOR_WEIGHT*(DOOR_GOAL_ - sim.data.qpos[-2])**2 + \
+		 HANDLE_WEIGHT*(HANDLE_GOAL_CLOSED - sim.data.qpos[-1])**2 + \
+		 GRASP_WEIGHT*(gripper_handle_distance)
+	return cost
 
 def seed_properly(seed_value):
 	os.environ['PYTHONHASHSEED']=str(seed_value)

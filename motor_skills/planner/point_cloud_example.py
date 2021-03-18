f259b51040e9c61e46853fdf48fa6eb183824f79
Matt <matt.corsaro1@gmail.com>
2021-03-18 14:45:23 -0400

    Using primesensedefaults seem to work better.

--- motor_skills/planner/point_cloud_example.py
+++ motor_skills/planner/point_cloud_example.py
@@ -11,11 +11,12 @@ def generatePointCloud():
     # sim.model.cam_fovy[0] = 60
     fovy = math.radians(60)
     fovx = 2 * math.atan(math.tan(fovy / 2) * aspect_ratio)
-    fx = 1/math.tan(fovx/2.0)
-    fy = 1/math.tan(fovy/2.0)
+    fx = 1/math.tan(fovx/2.0)*1000
+    fy = 1/math.tan(fovy/2.0)*1000
     cx = img_width/2
     cy = img_height/2
-    cam_mat = o3d.camera.PinholeCameraIntrinsic(img_width, img_height, fx, fy, cx, cy)
+    cam_mat = o3d.camera.PinholeCameraIntrinsic(o3d.camera.PinholeCameraIntrinsicParameters.PrimeSenseDefault)#img_width, img_height, fx, fy, cx, cy)
+    print("Cam mat\n", cam_mat.intrinsic_matrix)
 
     depth_img = captureImage()
 
@@ -28,7 +29,7 @@ def generatePointCloud():
 def captureImage():
     #img, depth = sim.render(img_width, img_height, camera_name=sim.model.camera_names[0], depth=True)
     # 480x640 np array
-    depth = np.loadtxt("/home/mcorsaro/depth_image_rendered.npy").astype(np.float32)
+    depth = np.loadtxt("/home/mcorsaro/depth_image_rendered_0.npy").astype(np.float32)
 
     flipped_depth = np.flip(depth, axis=0)
     real_depth = depthimg2Meters(flipped_depth)
@@ -47,12 +48,3 @@ def depthimg2Meters(depth):
 
 if __name__ == '__main__':
     generatePointCloud()
-
-def scaleCloudXY(cloud):
-    xy_scaler = np.array([1/1000., 1/1000., 1.])
-
-    np_cloud = np.asarray(cloud.points)
-    scaled_np_cloud = np_cloud*xy_scaler
-    scaled_cloud = o3d.geometry.PointCloud()
-    scaled_cloud.points = o3d.utility.Vector3dVector(scaled_np_cloud)
-    return scaled_cloud

23fbdf47fb63cccdc443e75d3c3ca110eecdb98d
Matt <matt.corsaro1@gmail.com>
2021-04-20 14:42:45 -0400

    autoencoder w/ tf 2.4

--- motor_skills/planner/autoencoder_test.py
+++ motor_skills/planner/autoencoder_test.py
@@ -1,12 +1,8 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-import tensorflow as tf
 
-from sklearn.metrics import accuracy_score, precision_score, recall_score
-from sklearn.model_selection import train_test_split
+import tensorflow as tf
 from tensorflow.keras import layers, losses
-from tensorflow.keras.datasets import fashion_mnist
 from tensorflow.keras.models import Model
 
 x0 = np.loadtxt("/home/mcorsaro/Desktop/0.txt")
@@ -14,6 +10,8 @@ x1 = np.loadtxt("/home/mcorsaro/Desktop/1.txt")
 
 latent_dim = 2
 original_dim = 6
+
+print("Using GPU", tf.config.list_physical_devices('GPU'))
 
 class Autoencoder(Model):
     def __init__(self, latent_dim):
@@ -33,7 +31,7 @@ class Autoencoder(Model):
         decoded = self.decoder(encoded)
         return decoded
 
-    def compress(self, x):
+    def transform(self, x):
         encoded = self.encoder(x)
         return encoded
 
@@ -41,36 +39,32 @@ class Autoencoder(Model):
         decoded = self.decoder(y)
         return decoded
 
-sess = tf.Session()
+autoencoder0 = Autoencoder(latent_dim)
+autoencoder1 = Autoencoder(latent_dim)
 
-with sess.as_default():
+autoencoder0.compile(optimizer='adam', loss=losses.MeanSquaredError())
+autoencoder1.compile(optimizer='adam', loss=losses.MeanSquaredError())
 
-    autoencoder0 = Autoencoder(latent_dim)
-    autoencoder1 = Autoencoder(latent_dim)
+print("Input", x0.shape, x1.shape)
 
-    autoencoder0.compile(optimizer='adam', loss=losses.MeanSquaredError())
-    autoencoder1.compile(optimizer='adam', loss=losses.MeanSquaredError())
+autoencoder0.fit(x0, x0,
+                epochs=10,
+                shuffle=True)
 
-    print("Input", x0.shape, x1.shape)
+autoencoder1.fit(x1, x1,
+                epochs=10,
+                shuffle=True)
+                #validation_data=(x_test, x_test))
 
-    autoencoder0.fit(x0, x0,
-                    epochs=10,
-                    shuffle=True)
+x01 = autoencoder0.transform(x1).numpy()
+x10 = autoencoder1.transform(x0).numpy()
+print("Low-D", x01.shape, x10.shape)
 
-    autoencoder1.fit(x1, x1,
-                    epochs=10,
-                    shuffle=True)
-                    #validation_data=(x_test, x_test))
+y2 = np.random.random((100, 2))
+y3 = np.random.random((100, 2))
 
-    x01 = autoencoder0.compress(x1).eval()
-    x10 = autoencoder1.compress(x0).eval()
-    print("Low-D", x01.shape, x10.shape)
-
-    y2 = np.random.random((100, 2))
-    y3 = np.random.random((100, 2))
-
-    x2 = autoencoder0.inverseTransform(y2).eval()
-    x3 = autoencoder1.inverseTransform(y3).eval()
-    print("Reconstructed", x2.shape, x3.shape)
+x2 = autoencoder0.inverseTransform(y2).numpy()
+x3 = autoencoder1.inverseTransform(y3).numpy()
+print("Reconstructed", x2.shape, x3.shape)
 
 print("done")
